818126e9e4f92f478b4b4debdfe7689f5764e529
Tatyana Lee <Tatyana Lee>
2016-10-14 14:37:22 -0500

    Movement

--- Sketch.js
+++ Sketch.js
@@ -1,25 +1,29 @@
+var x = 100
 var y = 400;
 var w = 20;
 
-function setup() {
+function setup () {
   createCanvas(800, 800);
 }
 
 function draw() {
   background(150, 16, 200);
-  fill(225);
-  ellipse(100, 400, 50, 50);
-
+//the dots
   fill(0, 0, 150);
   ellipse(200, y, w, w);
   ellipse(300, y, w, w);
   ellipse(400, y, w, w);
   ellipse(500, y, w, w);
   ellipse(600, y, w, w);
-}
-
-function keyPressed() {
-  if (keyCode == RIGHT_ARROW) {
-    
+  // the pacman movement
+  fill(225);
+  if (keyIsPressed) {
+    if (keyCode == RIGHT_ARROW) {
+      x++;
     }
+    else if (keyCode == LEFT_ARROW) {
+      x--;
+    }
+  }
+  ellipse(x, 400, 50, 50);
 }
